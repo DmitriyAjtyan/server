da4131a5071fe9884d90c9e819f9620eaf69f062
DmitriyAjtyan <ajtyand@mail.ru>
2019-09-09 01:01:08 +0300

    add jwt token check and new html template

--- back/route/route.go
+++ back/route/route.go
@@ -1,9 +1,12 @@
 package route
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	config "server/back/config"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
@@ -20,6 +23,9 @@ func RouterFunc() {
 
 // HomePageHandler func is a handler for "/" route
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	var secretKey = []byte(config.ConfigData.JWTSecret)
+	token := jwt.New(jwt.SigningMethodHS256)
+	tokenString, _ := token.SignedString(secretKey)
 	t, err := template.ParseFiles("../front/index.html")
 	if err != nil {
 		http.Error(w, err.Error(), 400)
@@ -27,15 +33,37 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	if err := t.Execute(w, nil); err != nil {
 		http.Error(w, err.Error(), 400)
 	}
+	w.Write([]byte(tokenString))
 }
 
 // ApiFirstHandler func is a handler for "/api/first" route
 func apiFirstHandler(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("../front/apiFirst.html")
+	r.ParseForm()
+	userJWTstring := r.Form["jwt"][0]
+	token, err := jwt.Parse(userJWTstring, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(config.ConfigData.JWTSecret), nil
+	})
 	if err != nil {
-		http.Error(w, err.Error(), 400)
+		fmt.Print("JWT token parse error: ", err)
 	}
-	if err := t.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), 400)
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		t, err := template.ParseFiles("../front/apiFirst.html")
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+		}
+		if err := t.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), 400)
+		}
+	} else {
+		t, err := template.ParseFiles("../front/apiSecond.html")
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+		}
+		if err := t.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), 400)
+		}
 	}
 }
